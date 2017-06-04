2ea7791fc0e93ca7dfd98f1f09133fd4e363b522
Sean Kallaher <seanp@kallaher.org>
2017-06-03 17:43:26 -0700

    Added script to split *.darknet files into correct .txt(s) in desired output directory

---
+++ sloth/sloth_to_darknet.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+
+def extract_data(fname):
+    pairs = []
+    with open(fname, 'r') as f:
+        filename = ""
+        text = []
+        trashingHeader = False
+        for line in f:
+            if "---" in line and not trashingHeader:
+                trashingHeader = True
+            elif "---" in line:
+                break
+            else:
+                pass
+        for line in f:
+            if ">>" in line:
+                filename = line.split(" ")[1][1:-2]
+            elif "<<" in line:
+               pairs.append((filename, text))
+               text = []
+               filename = ""
+            else:
+                text.append(line) 
+    return pairs
+
+def write_to_file(fname, text):
+    print("Writing to {}".format(fname))
+    with open(fname, 'w') as f:
+        for line in text:
+            f.write(line)
+
+def create_training_list(tlist, outname):
+    with open(outname, 'w') as f:
+        for line in tlist:
+            f.write(line)
+            f.write("\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert from sloth *.darknet format, to proper darknet used files")
+    parser.add_argument("--filename", "-f", type=str, help="A relative path to the *.darknet file", required=True)
+    parser.add_argument("--output-dir","-o", type=str, help="the path where the annotation files will be saved", required=True)
+    parser.add_argument("--training-name", "-t", type=str, help="The filename of the output training list", default="training-list.txt")
+
+    args = parser.parse_args()
+    print(args)
+
+    data = extract_data(args.filename)
+
+    training_list = []
+    for f, text in data:
+        filename = ".".join(f.split(".")[0:-1]) + ".txt"
+        training_list.append(os.path.normpath(os.path.join(os.path.abspath(os.path.split(args.filename)[0]), f)))
+        write_to_file(os.path.normpath(args.output_dir + filename), text)
+
+    print(training_list)
+    create_training_list(training_list, args.training_name)
