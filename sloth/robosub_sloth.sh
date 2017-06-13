ed2f8e4e21a2288e58cfa1951a1bf307c4f86e83
Sean Kallaher <seanp@kallaher.org>
2017-06-13 12:48:11 -0700

    Minor fixes to robosub_sloth.sh

--- sloth/robosub_sloth.sh
+++ sloth/robosub_sloth.sh
@@ -7,12 +7,10 @@ SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
     SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /* ]] && SOURCE="$TEST_DIR/$SOURCE" # if SOURCE was a relative symlink, resolve it
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if SOURCE was a relative symlink, resolve it
 done
 
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
-echo "$DIR"
 
 if [ -z "$DIR" ] ; then
     echo "Cannot access filepath: \"$DIR\""
