cfe7402a99540d10cad12d9f7b4601594001b483
Sindorman <mikebykhovtsev@gmail.com>
2017-07-25 00:27:51 -0700

    Fixed url for download to http://robosub-vm.eecs.wsu.edu/data/2017/vision/labeling/labeling_todo

--- download_data.sh
+++ download_data.sh
@@ -10,7 +10,7 @@
 #   you can run ./download_data.sh initial/red_buoy_sub_00 0 1 3
 
 
-BASE_URL='http://robosub.eecs.wsu.edu/data/2017/vision_labeling'
+BASE_URL='http://robosub-vm.eecs.wsu.edu/data/2017/vision/labeling/labeling_todo'
 SUB_PATH=$1
 
 if [ "$SUB_PATH" == "" ] ; then
