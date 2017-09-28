316aba2dc46d1416b6b418ecb8128cf8ae4b4d19
Sindorman <mikebykhovtsev@gmail.com>
2017-09-28 02:28:56 -0700

    Fixed syntax errors. Script still doesn't work properly. Need to figure out proper format of image publishing

--- labler.py
+++ labler.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import os
 import rospy
@@ -8,23 +8,26 @@ import numpy as np
 import cv2
 from rs_yolo.msg import DetectionArray
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 File = None
 counter = None
-Image = None
+image = None
 Output = None
 
 class labeler():
-    def __init__(self, image):
-        if counter == 0
+    def __init__(self):
+        if counter == 0:
             with open('data.json','w') as outfile:
                 json.dump('[', outfile)
 
-        image = self.image
         self.sub = rospy.Subscriber('/vision', DetectionArray, self.callback)
         self.pub = rospy.Publisher('/camera/left/undistorted', Image, queue_size=1)
 
-        if counter == len(File)
+        # Needs from image conversion from CV2 to Ros message Image.
+        bridge = CvBridge()
+
+        if counter == len(File):
             rospy.loginfo("Program Complete, shutting down.")
             with open('data.json', 'w') as outfile:
                 json.dump(']', outfile)
@@ -34,8 +37,8 @@ class labeler():
     def callback(self, detectionArray):
         image_width , image_height = cv.GetSize(image)
 
-        #using Mat image = load image from file name
-        for i in detectionArray
+        # using Mat image = load image from file name
+        for i in detectionArray:
 
             detection = detectionArray.detections[i]
 
@@ -47,25 +50,26 @@ class labeler():
 
             # get box dimensions
             x = (x - detection.width)
-            if x < 0
+            if x < 0:
                 left = 0
 
             y = (y - detection.height)
-            if y < 0
+            if y < 0:
                 y = 0
 
             output.insert(i, {'class': detection.label, 'height': height,
                 'type':'rect', 'width': width, 'x': x, 'y': y})
 
         json.dump({'annotations': [output], 'class': 'image',
-            'filename': File[counter]}, sort_keys=True, indent=4, outfile)
+            'filename': File[counter]}, outfile, sort_keys=True, indent=4, )
 
         output = None
 
-        if counter != len(File)
-            counter++
-            Image = cv2.imread(File[counter])
-            self.pub.publish(Image)
+        if counter != len(File):
+            counter += 1
+            Image = cv2.imread(sys.argv[1] + '/' + File[counter])
+            image_message = bridge.cv2_to_imgmsg(image, encoding="passthrough")
+            self.pub.publish(image_message)
 
 def printUsage():
     print("Invalid arguments")
@@ -73,14 +77,20 @@ def printUsage():
     print("\t./labeler.py (insert path to the folder here)")
 
 if __name__ == "__main__":
-    labeler = labeler()
+    rospy.init_node('labeler')
 #If path was not provided as an argument exit.
-    if sys.argv[0] == ""
+    if len(sys.argv) == 1:
         printUsage()
         exit(1)
-    File = os.listdir(argv[0)
+    File = os.listdir(sys.argv[1])
     counter = 0
-    Image = cv2.imread(File[0])
+    bridge = CvBridge()
+    image = cv2.imread(sys.argv[1] + '/' + File[0])
+    #bitmap = cv.CreateImageHeader((image.shape[1], image.shape[0]), cv.IPL_DEPTH_8U, 3)
+#cv.SetData(bitmap, image.tostring(), image.dtype.itemsize * 3 * image.shape[1])
     rospy.loginfo("Listening to /vision for detection array and publishing to /camera/left/undistorted as Mat image")
-    pub = rospy.Publisher('/camera/left/undistorted', Image)
+    image_message = bridge.cv2_to_imgmsg(image, encoding="passthrough")
+    pub = rospy.Publisher('/camera/left/undistorted', Image, queue_size=1)
+    pub.publish(image_message)
+    labeler = labeler()
     rospy.spin()
