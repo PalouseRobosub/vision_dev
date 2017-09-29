f6a68211634229e488b6c14dfbaa09a060740211
Sindorman <mikebykhovtsev@gmail.com>
2017-09-28 23:07:55 -0700

    More fixes, still requires more fixing.

--- labler.py
+++ labler.py
@@ -6,6 +6,7 @@ import sys
 import json
 import numpy as np
 import cv2
+from std_msgs.msg import String
 from rs_yolo.msg import DetectionArray
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -17,30 +18,38 @@ Output = None
 
 class labeler():
     def __init__(self):
-        if counter == 0:
-            with open('data.json','w') as outfile:
-                json.dump('[', outfile)
 
         self.sub = rospy.Subscriber('/vision', DetectionArray, self.callback)
         self.pub = rospy.Publisher('/camera/left/undistorted', Image, queue_size=1)
 
-        # Needs from image conversion from CV2 to Ros message Image.
+        # Needs for image conversion from CV2 to Ros message Image.
         bridge = CvBridge()
 
-        if counter == len(File):
-            rospy.loginfo("Program Complete, shutting down.")
-            with open('data.json', 'w') as outfile:
-                json.dump(']', outfile)
-            exit(0)
+        if counter == 0:
+            print(sys.argv[1] + '/' + File[counter])
+            image = cv2.imread(sys.argv[1] + '/' + File[counter])
+            print(image)
+            print("Hello")
+            self.image_message = bridge.cv2_to_imgmsg(image, "bgr8")
+            print(File[counter])
+            print(self.image_message)
+            self.pub.publish(self.image_message)
+
+            with open('data.json','w') as outfile:
+                json.dump('[', outfile)
+
+
 
 # Process the Image with labels
     def callback(self, detectionArray):
-        image_width , image_height = cv.GetSize(image)
+
+        image_width = self.image_message.width
+        image_height = self.image_message.height
 
         # using Mat image = load image from file name
-        for i in detectionArray:
+        for i in detectionArray.detections: #TODO fix for loop to use integer, now uses i as detection.detections[i]
 
-            detection = detectionArray.detections[i]
+            detection = i
 
             x = detection.x * image_width
             y = detection.y * image_height
@@ -59,17 +68,23 @@ class labeler():
 
             output.insert(i, {'class': detection.label, 'height': height,
                 'type':'rect', 'width': width, 'x': x, 'y': y})
-
-        json.dump({'annotations': [output], 'class': 'image',
-            'filename': File[counter]}, outfile, sort_keys=True, indent=4, )
+        with open('data.json','w') as outfile:
+            json.dump({'annotations': [output], 'class': 'image',
+                'filename': File[counter]}, outfile, sort_keys=True, indent=4, )
 
         output = None
 
         if counter != len(File):
             counter += 1
-            Image = cv2.imread(sys.argv[1] + '/' + File[counter])
-            image_message = bridge.cv2_to_imgmsg(image, encoding="passthrough")
-            self.pub.publish(image_message)
+            image = cv2.imread(sys.argv[1] + '/' + File[counter])
+            self.image_message = bridge.cv2_to_imgmsg(image, "bgr8")
+            self.pub.publish(self.image_message)
+
+        if counter == len(File):
+            rospy.loginfo("Program Complete, shutting down.")
+            with open('data.json', 'w') as outfile:
+                json.dump(']', outfile)
+            exit(0)
 
 def printUsage():
     print("Invalid arguments")
@@ -84,13 +99,8 @@ if __name__ == "__main__":
         exit(1)
     File = os.listdir(sys.argv[1])
     counter = 0
-    bridge = CvBridge()
-    image = cv2.imread(sys.argv[1] + '/' + File[0])
-    #bitmap = cv.CreateImageHeader((image.shape[1], image.shape[0]), cv.IPL_DEPTH_8U, 3)
-#cv.SetData(bitmap, image.tostring(), image.dtype.itemsize * 3 * image.shape[1])
+
     rospy.loginfo("Listening to /vision for detection array and publishing to /camera/left/undistorted as Mat image")
-    image_message = bridge.cv2_to_imgmsg(image, encoding="passthrough")
-    pub = rospy.Publisher('/camera/left/undistorted', Image, queue_size=1)
-    pub.publish(image_message)
+
     labeler = labeler()
     rospy.spin()
