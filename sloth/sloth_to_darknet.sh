da9c4a63685d704ef022b261b6da7ceedf20be39
Peter Brennan <peter.brennan@wsu.edu>
2017-10-10 19:00:43 -0700

    Wrote script that combines sloth_to_darknet.py and sloth convert

---
+++ sloth/sloth_to_darknet.sh
@@ -0,0 +1,14 @@
+json=${1}
+output=${2}
+
+folder_path_json=$(dirname "$json")
+filename_json=$(basename "$json")
+config="robosub_config.py"
+
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+sloth convert -c $config --pythonpath="$DIR" $json $folder_path_json/dataset.darknet
+
+./sloth_to_darknet.py -f $folder_path_json/dataset.darknet -o dataset
+tar -cf ${output}.tar dataset
