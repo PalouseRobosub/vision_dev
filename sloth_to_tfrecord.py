93da63cc95af27ff500cfa7c283a271e3ea10308
James Irwin <irwineffect@gmail.com>
2018-02-05 12:25:22 -0800

    fixed lots of bugs with sloth_to_tfrecord
    plus some cleanup

--- sloth_to_tfrecord.py
+++ sloth_to_tfrecord.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
-"""Convert Pascal VOC 2007+2012 detection dataset to TFRecords.
-Does not preserve full XML annotations.
-Combines all VOC 2007 subsets (train, val) with VOC2012 for training.
-Uses VOC2012 val for val and VOC2007 test for test.
-
+"""
 Code based on:
-https://github.com/pjreddie/darknet/blob/master/scripts/voc_label.py
-https://github.com/tensorflow/models/blob/master/inception/inception/data/build_image_data.py
+
+http://github.com/allanzelener/YAD2K/blob/master/voc_conversion_scripts/voc_to_tfrecords.py
+http://github.com/tensorflow/models/blob/master/research/object_detection/dataset_tools/create_pet_tf_record.py
 """
 
 import os
+import io
+import hashlib
+import PIL
 import sys
-import xml.etree.ElementTree as ElementTree
 from datetime import datetime
 from progress.bar import IncrementalBar as Bar
 import random
 
 import numpy as np
 import json
+from object_detection.utils import dataset_util
 
 import argparse
 
@@ -39,14 +39,14 @@ import tensorflow as tf
 # Small graph for image decoding
 decoder_sess = tf.Session()
 image_placeholder = tf.placeholder(dtype=tf.string)
-decoded_png = tf.image.decode_png(image_placeholder, channels=3)
+decoded_jpg = tf.image.decode_jpeg(image_placeholder, channels=3)
 
 
 def process_image(image_path):
     """Decode image at given path."""
     with open(image_path, 'rb') as f:
         image_data = f.read()
-    image = decoder_sess.run(decoded_png,
+    image = decoder_sess.run(decoded_jpg,
                              feed_dict={image_placeholder: image_data})
     assert len(image.shape) == 3
     height = image.shape[0]
@@ -54,35 +54,7 @@ def process_image(image_path):
     assert image.shape[2] == 3
     return image_data, height, width
 
-
-def process_anno(anno_path):
-    """Process Pascal VOC annotations."""
-    with open(anno_path) as f:
-        xml_tree = ElementTree.parse(f)
-    root = xml_tree.getroot()
-    size = root.find('size')
-    height = float(size.find('height').text)
-    width = float(size.find('width').text)
-    boxes = []
-    for obj in root.iter('object'):
-        difficult = obj.find('difficult').text
-        label = obj.find('name').text
-        if label not in classes or int(
-                difficult) == 1:  # exclude difficult or unlisted classes
-            continue
-        xml_box = obj.find('bndbox')
-        bbox = {
-            'class': classes.index(label),
-            'y_min': float(xml_box.find('ymin').text) / height,
-            'x_min': float(xml_box.find('xmin').text) / width,
-            'y_max': float(xml_box.find('ymax').text) / height,
-            'x_max': float(xml_box.find('xmax').text) / width
-        }
-        boxes.append(bbox)
-    return boxes
-
-
-def convert_to_example(image_data, boxes, filename, height, width):
+def convert_to_example(image_path, boxes):
     """Convert Pascal VOC ground truth to TFExample protobuf.
 
     Parameters
@@ -103,96 +75,50 @@ def convert_to_example(image_data, boxes, filename, height, width):
     example : protobuf
         Tensorflow Example protobuf containing image and bounding boxes.
     """
-    box_classes = [b['class'] for b in boxes]
-    box_ymin = [b['y_min'] for b in boxes]
-    box_xmin = [b['x_min'] for b in boxes]
-    box_ymax = [b['y_max'] for b in boxes]
-    box_xmax = [b['x_max'] for b in boxes]
-    encoded_image = [tf.compat.as_bytes(image_data)]
-    base_name = [tf.compat.as_bytes(os.path.basename(filename))]
+    with tf.gfile.GFile(image_path, 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = PIL.Image.open(encoded_jpg_io)
+    if image.format != 'JPEG':
+        raise ValueError('Image format not jpeg')
+    key = hashlib.sha256(encoded_jpg).hexdigest()
+
+    (width, height) = image.size
+
+    class_label = [b['class_label'].encode('utf8') for b in boxes]
+    class_index = [b['class_index'] for b in boxes]
+    ymin = [b['y_min'] for b in boxes]
+    xmin = [b['x_min'] for b in boxes]
+    ymax = [b['y_max'] for b in boxes]
+    xmax = [b['x_max'] for b in boxes]
 
     example = tf.train.Example(features=tf.train.Features(feature={
-        'filename':
-        tf.train.Feature(bytes_list=tf.train.BytesList(value=base_name)),
-        'height':
-        tf.train.Feature(int64_list=tf.train.Int64List(value=[height])),
-        'width':
-        tf.train.Feature(int64_list=tf.train.Int64List(value=[width])),
-        'classes':
-        tf.train.Feature(int64_list=tf.train.Int64List(value=box_classes)),
-        'y_mins':
-        tf.train.Feature(float_list=tf.train.FloatList(value=box_ymin)),
-        'x_mins':
-        tf.train.Feature(float_list=tf.train.FloatList(value=box_xmin)),
-        'y_maxes':
-        tf.train.Feature(float_list=tf.train.FloatList(value=box_ymax)),
-        'x_maxes':
-        tf.train.Feature(float_list=tf.train.FloatList(value=box_xmax)),
-        'encoded':
-        tf.train.Feature(bytes_list=tf.train.BytesList(value=encoded_image))
-    }))
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(
+            os.path.basename(image_path).encode('utf8')),
+        'image/source_id': dataset_util.bytes_feature(
+            os.path.basename(image_path).encode('utf8')),
+        'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
+        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+        'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
+        'image/object/class/text': dataset_util.bytes_list_feature(class_label),
+        'image/object/class/label': dataset_util.int64_list_feature(class_index),
+        }))
+
     return example
-
-def process_dataset(name, image_paths, anno_paths, result_path, num_shards):
-    """Process selected Pascal VOC dataset to generate TFRecords files.
-
-    Parameters
-    ----------
-    name : string
-        Name of resulting dataset 'train' or 'test'.
-    image_paths : list
-        List of paths to images to include in dataset.
-    anno_paths : list
-        List of paths to corresponding image annotations.
-    result_path : string
-        Path to put resulting TFRecord files.
-    num_shards : int
-        Number of shards to split TFRecord files into.
-    """
-    shard_ranges = np.linspace(0, len(image_paths), num_shards + 1).astype(int)
-    counter = 0
-    for shard in range(num_shards):
-        # Generate shard file name
-        output_filename = '{}-{:05d}-of-{:05d}'.format(name, shard, num_shards)
-        output_file = os.path.join(result_path, output_filename)
-        writer = tf.python_io.TFRecordWriter(output_file)
-
-        shard_counter = 0
-        files_in_shard = range(shard_ranges[shard], shard_ranges[shard + 1])
-        for i in files_in_shard:
-            image_file = image_paths[i]
-            anno_file = anno_paths[i]
-
-            # processes image + anno
-            image_data, height, width = process_image(image_file)
-            boxes = process_anno(anno_file)
-
-            # convert to example
-            example = convert_to_example(image_data, boxes, image_file, height,
-                                         width)
-
-            # write to writer
-            writer.write(example.SerializeToString())
-
-            shard_counter += 1
-            counter += 1
-
-            if not counter % 1000:
-                print('{} : Processed {:d} of {:d} images.'.format(
-                    datetime.now(), counter, len(image_paths)))
-        writer.close()
-        print('{} : Wrote {} images to {}'.format(
-            datetime.now(), shard_counter, output_filename))
-
-    print('{} : Wrote {} images to {} shards'.format(datetime.now(), counter,
-                                                     num_shards))
 
 def extract_boxes(annotations, img_height, img_width, classes):
 
     boxes = list()
     for annotation in annotations:
         # tfrecord wants numerical index for class labels
-        label_index = classes.index(str(annotation['class']))
+        class_label = str(annotation['class'])
+        class_index = classes.index(class_label)
         x = annotation['x']
         y = annotation['y']
         width = annotation['width']
@@ -218,7 +144,8 @@ def extract_boxes(annotations, img_height, img_width, classes):
         assert (ymax >= 0.0) and (ymax <= 1.0), "ymax: {}".format(ymax)
 
         box = {
-                'class': label_index,
+                'class_label': class_label,
+                'class_index': class_index,
                 'y_min': ymin,
                 'x_min': xmin,
                 'y_max': ymax,
@@ -239,7 +166,19 @@ def find_classes(json_data):
     classes.sort()
     return classes
 
+def write_label_map(classes, outfile):
+    f = open(outfile, "w")
+    for index, label in enumerate(classes, 1):
+        f.write("item {\n")
+        f.write("  id:{}\n".format(index))
+        f.write("  name: '{}'\n".format(label))
+        f.write("}\n\n")
+
 def create_record(outfile, json_data, classes, input_path, progress):
+    # Record keeping
+    processed = 0
+    skipped_not_good = 0
+    skipped_not_found = 0
     writer = tf.python_io.TFRecordWriter(outfile)
     #counter = 0
     for item in json_data:
@@ -247,22 +186,30 @@ def create_record(outfile, json_data, classes, input_path, progress):
         #counter += 1
         progress.next()
 
+        # Skip files marked as bad
+        if str(item["status"]) != "Good":
+            skipped_not_good += 1
+            continue
+
         filepath = os.path.join(input_path, str(item['filename']))
         filename = os.path.basename(filepath)
         try:
             image_data, height, width = process_image(filepath)
         except IOError:
             #print "could not find {}, skipping".format(filepath)
+            skipped_not_found += 1
             continue
+
         boxes = extract_boxes(item['annotations'],
                 img_height=height,
                 img_width=width,
                 classes=classes)
 
-        tfexample = convert_to_example(image_data, boxes, filename,
-                height, width)
+        tfexample = convert_to_example(filepath, boxes)
 
         writer.write(tfexample.SerializeToString())
+        processed += 1
+    return processed, skipped_not_good, skipped_not_found
 
 
 def _main(args):
@@ -271,8 +218,9 @@ def _main(args):
     input_path = os.path.expanduser(input_path)
     output_path = args.outdir
 
-    train_path = os.path.join(output_path, 'train.tfrecord')
-    test_path = os.path.join(output_path, 'test.tfrecord')
+    train_path = os.path.join(output_path, 'train.record')
+    test_path = os.path.join(output_path, 'test.record')
+    label_path = os.path.join(output_path, 'label_map.pbtxt')
 
     json_data = json.load(open(args.input, 'r'))
 
@@ -282,6 +230,8 @@ def _main(args):
         print "  {}".format(x)
     print ""
 
+    write_label_map(classes, label_path)
+    print "label map saved to {}".format(label_path)
 
     # shuffle our data, split into training and testing segments
     random.shuffle(json_data)
@@ -289,18 +239,29 @@ def _main(args):
     training_data = json_data[0 : split_index]
     testing_data = json_data[split_index:]
 
+    # Create the training data
     progress = Bar('Creating training data', max=len(training_data),
             suffix='%(percent)d%%')
-    create_record(train_path, training_data, classes, os.path.dirname(input_path), progress)
+    processed1, skipped_not_good1, skipped_not_found1 = \
+        create_record(train_path, training_data, classes,
+                os.path.dirname(input_path), progress)
     progress.finish()
-    print "training data saved to {}".format(train_path)
+    print "Training data saved to {}.".format(train_path)
 
+    # Create the testing data
     progress = Bar('Creating testing data ', max=len(testing_data),
             suffix='%(percent)d%%')
-    create_record(test_path, testing_data, classes, os.path.dirname(input_path), progress)
-
+    processed2, skipped_not_good2, skipped_not_found2 = \
+    create_record(test_path, testing_data, classes,
+            os.path.dirname(input_path), progress)
     progress.finish()
-    print "testing data saved to {}".format(test_path)
+    print "Testing data saved to {}.".format(test_path)
+
+    # Print some file statistics
+    print "{} images successfuly processed".format(processed1+processed2)
+    print "{} images skipped for not being marked good".format(
+            skipped_not_good1+skipped_not_good2)
+    print "{} images not found".format(skipped_not_found1+skipped_not_found2)
 
 if __name__ == '__main__':
         _main(args)
