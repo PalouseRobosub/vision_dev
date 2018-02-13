5bfd71af9fb0c0d2e2c784f36430a2d7fb833b1b
Sindorman <mikebykhovtsev@gmail.com>
2018-02-12 21:58:54 -0800

    Added comments to the code.

--- get_bag.sh
+++ get_bag.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This program will check if there are any tars on the server and fetch one.
+# for use run just ./get_bag.sh
 
 # prompt for login information
 echo -n "user: "
@@ -8,19 +9,25 @@ read USER
 # fetch list of tars
 tar=$(ssh ${USER}@robosub.eecs.wsu.edu 'ls /data/vision/labeling/labeling_todo/current | grep .tar')
 tarArray=(${tar})
+
 # If there are no tars in the folder, exit.
 if [ "$tar" == "" ]; then
     echo -n "There are no tars available for download"
     exit
 fi
 
+# notify of all tars available and notify which will be downloaded
 echo -n 'Available tars for download: ' ${tar}
 echo
 echo -n "Grabbing: " ${tarArray[0]}
 echo
+
+# download tar
 rsync -Ph ${USER}@robosub.eecs.wsu.edu:/data/vision/labeling/labeling_todo/current/${tarArray[0]} "."
 echo -n "Moving tar to working_on folder"
 echo
+
+# move downloaded tar to working_on folder
 ssh ${USER}@robosub.eecs.wsu.edu 'mv /data/vision/labeling/labeling_todo/current/'${tarArray[0]} ' /data/vision/labeling/labeling_todo/current/working_on/'
 echo -n "done!"
 echo
--- upload_json.sh
+++ upload_json.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # This program will upload json to the server, and move tar to "done" folder
+# for use run ./upload_json.sh {json_name}
 
 name=${1}
 
+# check if name was given
 if [ "$1" == "" ]; then
     echo -n "Please name of .json file: "
     read name
@@ -13,10 +15,13 @@ fi
 echo -n "user: "
 read USER
 
+# rsync file to the server
 echo -n "Uploading .json to the server"
 echo
 rsync -Ph $name ${USER}@robosub.eecs.wsu.edu:/data/vision/labeling/labeling_todo/current/done/
 echo -n "Moving tar to done folder"
+
+# move tar with same name to done folder
 ssh ${USER}@robosub.eecs.wsu.edu 'mv /data/vision/labeling/labeling_todo/current/working_on/'${name%.json}.tar ' /data/vision/labeling/labeling_todo/current/done/'
 echo -n "Done!"
 echo
