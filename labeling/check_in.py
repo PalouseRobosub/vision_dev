078a226e580e4967a642d23954df0d92a5443ecf
Ryan Summers <summers.ryan.m@gmail.com>
2018-02-19 20:30:37 -0800

    Added checkin, get_data, and unbagger scripts

---
+++ labeling/check_in.py
@@ -0,0 +1,45 @@
+import argparse
+import pysftp
+import os
+import sys
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Check in Robosub image labels.')
+    parser.add_argument('password', type=str, help='The robosub SFTP password.')
+    parser.add_argument('annotations', type=str, help='The annotations file to upload.')
+    parser.add_argument('--validation', action='store_true')
+
+    args = parser.parse_args()
+
+    # Figure out the source and destination directory based upon input flag.
+    if args.validation:
+        dest_dir = 'done/'
+        src_dir = 'in_progress/validation/'
+    else:
+        dest_dir = 'unvalidated/'
+        src_dir = 'in_progress/labeling/'
+
+    with pysftp.Connection('robosub.eecs.wsu.edu',
+                username='sftp_user',
+                password=args.password,
+                default_path='/data/vision/labeling') as sftp:
+        with sftp.cd(src_dir):
+            tars = [x for x in sftp.listdir() if x.endswith('.tar')]
+
+        annotation_tar = os.path.splitext(os.path.basename(args.annotations))[0] + '.tar'
+        if annotation_tar not in tars:
+            print 'The provided annotations dataset was not found robosub.eecs.wsu.edu:/data/vision/labeling/{}'.format(src_dir)
+            sys.exit(-1)
+
+        # Upload the JSON to the server.
+        with sftp.cd(dest_dir):
+            sftp.put(args.annotations)
+
+        # Move the found tar into unvalidated/
+        sftp.rename('{}/{}'.format(src_dir, annotation_tar),
+                    '{}/{}'.format(dest_dir, annotation_tar))
+
+        # Remove the ownership and annotation file.
+        if args.validation:
+            sftp.remove('{}/{}'.format(src_dir, args.annotations))
+        sftp.remove('{}/{}.owner'.format(src_dir, annotation_tar))
---
+++ labeling/get_data.py
@@ -0,0 +1,88 @@
+import argparse
+import glob
+import json
+import pysftp
+import tarfile
+import tempfile
+import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get data for labeling.')
+    parser.add_argument('password', type=str, help='The SFTP password for the server.')
+    parser.add_argument('--validation', action='store_true')
+
+    args = parser.parse_args()
+
+    if args.validation:
+        src_dir = 'unvalidated/'
+        dest_dir = 'in_progress/validation/'
+    else:
+        src_dir = 'new/'
+        dest_dir = 'in_progress/labeling/'
+
+    data_owner = os.environ.get('ROBOSUB_WHO_AM_I')
+    if data_owner is None:
+        print 'To suppress this prompt, please set the environment variable ROBOSUB_WHO_AM_I to your name.'
+        data_owner = raw_input('Please enter your name (First Last): ')
+
+    with pysftp.Connection('robosub.eecs.wsu.edu',
+            username='sftp_user',
+            password=args.password,
+            default_path='/data/vision/labeling') as sftp:
+        with sftp.cd(src_dir):
+            tars = [x for x in sftp.listdir() if str(x).endswith('.tar')]
+
+        if len(tars) == 0:
+            print 'No data was available at robosub.eecs.wsu.edu:/data/vision/labeling/{}'.format(src_dir)
+            sys.exit(0)
+
+        tar = str(tars[0])
+
+        print 'Grabbing {}'.format(tar)
+
+        tar_name = os.path.splitext(tar)[0]
+
+        # Move tar to in_progress -> It's ours now.
+        sftp.rename(src_dir + tar, dest_dir + tar)
+
+        # Make an owner file on the server to denote that we own the tar.
+        owner_file = '{}.owner'.format(tar)
+        temp_dir = tempfile.mkdtemp()
+        with open('{}/{}'.format(temp_dir, owner_file), 'w') as f:
+            f.write(data_owner)
+
+        with sftp.cd(dest_dir):
+            sftp.put('{}/{}'.format(temp_dir, owner_file))
+
+        # Get the tar.
+        sftp.get(dest_dir + tar)
+
+        # Untar the data
+        with tarfile.TarFile(tar) as tf:
+            tf.extractall()
+
+        # If this is validation, grab the JSON from the server.
+        if args.validation:
+            tar_base = os.path.splitext(tar)[0]
+            json_name = tar_base + '.json'
+
+            sftp.rename(src_dir + json_name, dest_dir + json_name)
+            with sftp.cd(dest_dir):
+                print 'Grabbing {}'.format(json_name)
+                sftp.get(json_name, localpath='{}/{}'.format(tar_base, json_name))
+        else:
+            # Generate labeling JSON for data to begin labeling.
+            annotations = []
+            for f in glob.glob('{}/*.jpg'.format(tar_name)):
+                annotations.append({'annotations': [],
+                                    'class': 'image',
+                                    'filename': os.path.basename(f),
+                                    'unlabeled': True})
+
+            with open('{}/{}.json'.format(tar_name, tar_name), 'w') as f:
+                json.dump(annotations, f, indent=4)
+
+    # Remove the tar file - We don't need it anymore.
+    os.remove(tar)
+
+    print 'Images for available in {}'.format(tar_name)
