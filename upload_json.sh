a7df5de86c97b571bbac81ea11d8b69a4896e013
Sindorman <mikebykhovtsev@gmail.com>
2018-02-27 17:10:47 -0800

    Changed path to upload and download. Added use of sshpass to prompt password once.

--- upload_json.sh
+++ upload_json.sh
@@ -20,14 +20,16 @@ fi
 # prompt for login information
 echo -n "user: "
 read USER
+echo -n "password: "
+read -s PASS
 
 # rsync file to the server
 echo -n "Uploading .json to the server"
 echo
-rsync -Ph $name ${USER}@robosub.eecs.wsu.edu:/data/vision/labeling/labeling_todo/current/done/
+sshpass -p ${PASS} rsync -Ph $name ${USER}@robosub.eecs.wsu.edu:/data/vision/labeling/unvalidated
 echo -n "Moving tar to done folder"
 
 # move tar with same name to done folder
-ssh ${USER}@robosub.eecs.wsu.edu 'mv /data/vision/labeling/labeling_todo/current/working_on/'${name%.json}.tar ' /data/vision/labeling/labeling_todo/current/done/'
+sshpass -p ${PASS} ssh -o StrictHostKeyChecking=no ${USER}@robosub.eecs.wsu.edu 'mv /data/vision/labeling/in_progress/'${name%.json}.tar ' /data/vision/labeling/unvalidated/'
 echo -n "Done!"
 echo
