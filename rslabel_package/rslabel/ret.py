d31a8c170c27fa1b856fdfc95aa43d0307ea6763
Ryan Summers <summers.ryan.m@gmail.com>
2018-04-04 04:53:38 -0700

    Updated return script to add in history file for stat tracking

--- rslabel_package/rslabel/ret.py
+++ rslabel_package/rslabel/ret.py
@@ -8,11 +8,13 @@
 
 from __future__ import print_function
 
+import datetime
 import json
 import os
 import pysftp
 import shutil
 import sys
+import tempfile
 
 try:
     input = raw_input
@@ -27,6 +29,15 @@ def app(args):
         args: Passed in from argparse. Must have a named member `annotations`
               that specifies the path to the annotation JSON.
     """
+
+    # Grab the username for data ownership and the SFTP password.
+    data_owner = os.environ.get('ROBOSUB_WHO_AM_I')
+    if data_owner is None:
+        print('To suppress this prompt, please set the environment variable ',
+                end='')
+        print('ROBOSUB_WHO_AM_I to your name.')
+        data_owner = input('Please enter your name (First Last): ')
+
     password = os.environ.get('ROBOSUB_SFTP_PASSWORD')
     if password is None:
         print('To suppress this prompt, please set the ROBOSUB_SFTP_PASSWORD ',
@@ -113,6 +124,61 @@ def app(args):
             print('robosub.eecs.wsu.edu:/data/vision/labeling/' + src_dir)
             sys.exit(-1)
 
+        # If the annotations already exist on the server, pull them down to
+        # figure out what new labels were added for stat tracking.
+        previous_annotations = []
+        with sftp.cd(src_dir):
+            if os.path.basename(args.annotations) in sftp.listdir():
+                with tempfile.NamedTemporaryFile() as tempf:
+                    sftp.get(os.path.basename(args.annotations), tempf.name)
+                    with open(tempf.name, 'r') as f:
+                        previous_annotations = json.load(f)
+
+        with open(args.annotations, 'r') as f:
+            new_annotations = json.load(f)
+
+        if len(previous_annotations) and len(new_annotations) != len(previous_annotations):
+            print('Provided annotation file and server annotation file differ.')
+            sys.exit(-1)
+
+        log = {'labels_added': 0,
+               'images_labeled': 0,
+               'labels_validated': 0,
+               'images_validated': 0}
+
+        if len(previous_annotations) == 0:
+            for annotation in new_annotations:
+                if in_validation:
+                    log['images_validated'] += 1
+                    log['labels_validated'] += len(annotation['annotations'])
+                else:
+                    labels_added = len(annotation['annotations'])
+
+                    log['labels_added'] += labels_added
+                    if labels_added > 0:
+                        log['images_labeled'] += 1
+        else:
+            for old, new in zip(previous_annotations, new_annotations):
+                if in_validation:
+                    log['images_validated'] += 1
+                    try:
+                        status = new['status']
+                        try:
+                            old_status = old['status']
+                            if old_status != status:
+                                log['labels_validated'] += len(new['annotations'])
+                                log['images_validated'] += 1
+                        except:
+                            log['labels_validated'] += len(new['annotations'])
+                            log['images_validated'] += 1
+                    except:
+                        pass
+                else:
+                    labels_added = len(new['annotations']) - len(old['annotations'])
+                    if labels_added > 0:
+                        log['labels_added'] += labels_added
+                        log['images_labeled'] += 1
+
         # Upload the JSON to the server.
         with sftp.cd(dest_dir):
             sftp.put(args.annotations)
@@ -141,5 +207,28 @@ def app(args):
             if delete:
                 print('Deleting {}/'.format(directory))
                 shutil.rmtree(directory)
+
+        # Finally, upload the stats to the history folder on the
+        # server for stats tracking.
+        stats = {data_owner: log,
+                 'date-time': datetime.datetime.now().isoformat()}
+
+        # Only upload stats if someone has modified the annotations.
+        if log['images_labeled'] != 0 or log['images_validated'] != 0:
+            with tempfile.NamedTemporaryFile(prefix=data_owner) as tempf:
+                with open(tempf.name, 'w') as f:
+                    json.dump(stats, f)
+
+                with sftp.cd('history'):
+                    log_files = [x for x in sftp.listdir() if x.startswith(data_owner.replace(' ', '_'))]
+                    file_numbers = [int(os.path.splitext(x)[0].split('-')[-1]) for x in log_files]
+                    if len(file_numbers) == 0:
+                        f_number = 0
+                    else:
+                        f_number = max(file_numbers) + 1
+
+                    dst = os.path.basename(tempf.name)
+                    sftp.put(tempf.name)
+                    sftp.rename(dst, '{}-{}.log'.format(data_owner.replace(' ', '_'), f_number))
 
         print('Data has been successfully returned.')
